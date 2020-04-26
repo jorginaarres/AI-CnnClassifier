b6d4c4ecf7c286b4d2b9e5aeff50e4da48505ad6
1203217 <jorgina.arres@est.fib.upc.edu>
2020-04-26 11:20:06 +0200

    Add files to run the video tester and create the model

---
+++ modelCnn.py
@@ -0,0 +1,120 @@
+import pandas as pd
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D, MaxPooling2D, BatchNormalization
+from keras.optimizers import Adam
+from keras.utils import np_utils
+from keras.losses import categorical_crossentropy
+
+
+from keras.preprocessing.image import ImageDataGenerator
+
+df = pd.read_csv('./fer2013.csv')
+# print(df.info())
+
+X_train, train_y, X_test, test_y = [], [], [], []
+
+for index, row in df.iterrows():
+    val = row['pixels'].split(" ")
+    try:
+        if 'Training' in row['Usage']:
+            X_train.append(np.array(val, 'float32'))
+            train_y.append(row['emotion'])
+        elif 'PublicTest' in row['Usage']:
+            X_test.append(np.array(val, 'float32'))
+            test_y.append(row['emotion'])
+    except:
+        print(f"error occured at index :{index} and row:{row}")
+
+num_labels = 7
+batch_size = 64
+epochs = 30
+
+X_train = np.array(X_train, 'float32')
+train_y = np.array(train_y, 'float32')
+X_test = np.array(X_test, 'float32')
+test_y = np.array(test_y, 'float32')
+
+train_y = np_utils.to_categorical(train_y, num_classes=num_labels)
+test_y = np_utils.to_categorical(test_y, num_classes=num_labels)
+
+# cannot produce
+# normalizing data between oand 1
+X_train -= np.mean(X_train, axis=0)
+X_train /= np.std(X_train, axis=0)
+
+X_test -= np.mean(X_test, axis=0)
+X_test /= np.std(X_test, axis=0)
+
+X_train = X_train.reshape(X_train.shape[0], 48, 48, 1)
+
+X_test = X_test.reshape(X_test.shape[0], 48, 48, 1)
+
+# 1st convolution layer
+model = Sequential()
+
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', input_shape=(X_train.shape[1:])))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.5))
+
+# 2nd convolution layer
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.5))
+
+# 3rd convolution layer
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+model.add(Flatten())
+
+# fully connected neural networks
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.2))
+
+model.add(Dense(num_labels, activation='softmax'))
+
+print("MODEL SUMMARY: ")
+print(model.summary())
+
+# Compile the model
+model.compile(optimizer=Adam(), loss=categorical_crossentropy, metrics=["accuracy"])
+
+# Data Augmentation
+datagen = ImageDataGenerator(
+    featurewise_center=False,  # set input mean to 0 over the dataset
+    samplewise_center=False,  # set each sample mean to 0
+    featurewise_std_normalization=False,  # divide inputs by std of the dataset
+    samplewise_std_normalization=False,  # divide each input by its std
+    zca_whitening=False,  # apply ZCA whitening
+    rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
+    zoom_range=0.1,  # Randomly zoom image
+    width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+    # height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+    horizontal_flip=False,  # randomly flip images
+    vertical_flip=False)  # randomly flip images
+
+datagen.fit(X_train)
+
+# Training the model
+model.fit(X_train, train_y,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(X_test, test_y),
+          shuffle=True)
+
+# Saving the  model to  use it later on
+fer_json = model.to_json()
+with open("AIModel2.json", "w") as json_file:
+    json_file.write(fer_json)
+model.save_weights("AIModel2.h5")
